e8cf2c32b3caee22c91b1e9c2996ad063c807450
Atindriya Ghosh <atindriya@ghosh.me>
2014-11-16 11:59:44 +0530

    Adding search functionality

---
+++ assets/js/ghostHunter-init.js
@@ -0,0 +1,10 @@
+var searchField = $("#search-field").ghostHunter({
+            results   : "#results",
+            onKeyUp   : true,
+			info_template: "<span class='at_search_clear_toggle' onclick='clearResults()'>&#215;</span> <span class='at_search_results_amount'>Total Results: {{amount}}</span></span>",
+			result_template : "<span class='at_search_results_link'><a href='{{link}}'><span>{{title}}</span></a></span>"
+         });
+		 
+function clearResults(){
+	searchField.clear();
+}
