fb8350ed77bacb1557c767ec6f69e45646f24c72
Atindriya Ghosh <atindriya@ghosh.me>
2015-02-08 19:36:47 +0530

    Changing GhostHunter RSS path

--- assets/js/ghostHunter-init.js
+++ assets/js/ghostHunter-init.js
@@ -2,7 +2,8 @@ var searchField = $("#search-field").ghostHunter({
             results   : "#results",
             onKeyUp   : true,
 			info_template: "<span class='at_search_clear_toggle' onclick='clearResults()'>&#215;</span> <span class='at_search_results_amount'>Total Results: {{amount}}</span></span>",
-			result_template : "<span class='at_search_results_link'><a href='{{link}}'><span>{{title}}</span></a></span>"
+			result_template : "<span class='at_search_results_link'><a href='{{link}}'><span>{{title}}</span></a></span>",
+			rss: "/rss"
          });
 		 
 function clearResults(){
